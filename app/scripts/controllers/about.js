7eb78c0f061dbbc5a10014346f14cdfb2a2cff3c
mitvcu <aminm2@vcu.edu>
2017-02-18 20:59:07 -0500

    First Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name reachOutApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the reachOutApp
+ */
+angular.module('reachOutApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
